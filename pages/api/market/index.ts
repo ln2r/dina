ac3fd8979cc272212ed7992f893f1629ea0c061a
ln2r <scorch_62@live.com>
2023-03-05 20:32:32 +0700

    feat(api-market): add types for market item

--- pages/api/market/index.ts
+++ pages/api/market/index.ts
@@ -1,14 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import get from 'axios'
+import { ArsaMarketItemData } from '../types/items';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  console.debug('[API][get-market] running with params', req.query);
   // query.id = item id
   // query.region = game region
-  const market = await (await get(`https://api.arsha.io/v2/${req.query.region}/item?id=${req.query.id}&lang=en`)).data;  
+  const market: ArsaMarketItemData = await (await get(`https://api.arsha.io/v2/${req.query.region}/item?id=${req.query.id}&lang=en`)).data;  
 
-  res.status(200).send(market as any)
+  res.status(200).send(market);
 }
---
+++ pages/api/types/items.ts
@@ -0,0 +1,15 @@
+export type ArsaMarketItemData = {
+  name: string;
+  icon: string;
+  id: number;
+  sid: number;
+  minEnhance: string;
+  maxEnhance: string;
+  basePrice: number;
+  currentStock: number;
+  totalTrades: number;
+  priceMin: number;
+  priceMax: number;
+  lastSoldPrice: number;
+  lastSoldTime: number;
+}
