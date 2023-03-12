ac3f00acab96c3d2025d30579ac48ad8dd98f3a7
ln2r <scorch_62@live.com>
2023-03-12 18:26:14 +0700

    feat(site-market): add local storage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleXmark} from '@fortawesome/free-solid-svg-icons'
+import { faCircleXmark, faStar} from '@fortawesome/free-solid-svg-icons'
 import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons'
 import Head from 'next/head'
 import { Container, Row, Col, Form, Modal, Table, Spinner } from 'react-bootstrap'
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
 import { useEffect, useState } from 'react'
-import { getMarket } from '../utils/getMarket'
-import { getMarketHistorical } from '../utils/getMarketHistorical'
+import { getMarketData } from '../utils/getMarket'
+import { getHistoricalData } from '../utils/getMarketHistorical'
 import { debounce } from 'lodash'
 import axios from 'axios'
 import { NavbarComponent } from '../components/navbar'
 import { MarketCardComponent } from '../components/marketCard'
+import { ArsaMarketItemData } from './api/types/items'
 
 export default function Home() {
   ChartJS.register(
@@ -35,15 +36,6 @@ export default function Home() {
       },
     },
   };
-
-  type MarketData = {
-    name: string;
-    basePrice: number;
-    totalTrades: number;
-    priceMin: number;
-    priceMax: number;
-    lastSoldPrice: number;
-  };
   
   type HistoricalData = {
     labels: string[];
@@ -52,30 +44,27 @@ export default function Home() {
       data: number[];
     }[];
   };
+
+  type ItemModalMarketData = ArsaMarketItemData & {
+    saved: boolean;
+  }
   
   type ItemModalData = {
-    market?: MarketData;
+    market?: ItemModalMarketData;
     historical?: HistoricalData;
-  };
-
-  const getMarketData = async (id: number) => {
-    const response = await fetch(`/api/market?region=na&id=${id}`);
-    const data = await response.json();
-    return data;
-  };
-
-  const getHistoricalData = async (id: number) => {
-    const response = await fetch(`/api/market/history?region=na&id=${id}&sid=0`);
-    const data = await response.json();
-    return data;
   };
 
   const [show, setShow] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [savedLoading, setSavedLoading] = useState(false);
   const [itemModal, setItemModal] = useState<ItemModalData>({});
   
-  const handleClose = () => setShow(false);
+  const [bookmarkedItems, setBookmarkedItems] = useState<any>([]);
 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState<any>([]);
+  
+  const handleClose = () => setShow(false);
   const handleShow = async (id: number) => {
     setShow(true);
     setLoading(true);
@@ -84,7 +73,11 @@ export default function Home() {
       const marketData = await getMarketData(id);
       const historicalData = await getHistoricalData(id);
 
-      console.log(marketData);
+      // marking the saved items
+      const saved = JSON.parse(localStorage.getItem('items') as string);
+      if (saved && marketData) {
+        marketData[0].saved = saved.find((savedItem:any) => savedItem.id === marketData[0].id) ? true : false;
+      }
 
       setItemModal({
         market: marketData[0],
@@ -97,22 +90,6 @@ export default function Home() {
       setLoading(false);
     }
   };
-  const [savedDataPrimary, setSavedDataPrimary] = useState<any>([]);
-  const [savedDataSecondary, setSavedDataSecondary] = useState<any>([]);
-
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState<any>([]);
-
-  const savedItemsPrimary = [
-    { id: 4658, sid: 0 },
-    { id: 5802, sid: 0 },
-  ];
-
-  const savedItemsSecondary = [
-    { id: 4401, sid: 0 },
-    { id: 5856, sid: 0 },
-    { id: 4657, sid: 0 },
-  ]
 
   const searchItems = debounce(async (event) => {
     const inputValue = event.target.value;
@@ -121,6 +98,14 @@ export default function Home() {
     try {
       if (inputValue !== '') {
         const response = await axios.get(`/api/items?name=${inputValue}&limit=15`);
+
+        // marking the saved items
+        const saved = JSON.parse(localStorage.getItem('items') as string);
+        if (saved) {
+          response.data.map((item:any) => {
+            item.saved = saved.find((savedItem:any) => savedItem.id === item.id) ? true : false;
+          })
+        }
 
         setSearchResults(response.data);
       } else {
@@ -131,68 +116,76 @@ export default function Home() {
     }
   }, 500);
 
-  useEffect(() => {
-    async function getAPIData() {
-      const responsePrimary:any[] = [];
-      const responseSecondary:any[] = [];
-      for (const ids of savedItemsPrimary) {
-        responsePrimary.push({
-          historical: await getMarketHistorical(`/api/market/history?region=na&id=${ids.id}&sid=${ids.sid}`),
-          market: await (await getMarket(`/api/market?region=na&id=${ids.id}`))[0],
-        })
-      }
-
-      for (const ids of savedItemsSecondary) {
-        responseSecondary.push({
-          historical: await getMarketHistorical(`/api/market/history?region=na&id=${ids.id}&sid=${ids.sid}`),
-          market: await (await getMarket(`/api/market?region=na&id=${ids.id}`))[0],
-        })
-      }
-
-      setSavedDataPrimary(responsePrimary);
-      setSavedDataSecondary(responseSecondary);
+  const addToLocalStorage = (itemId: number | undefined, itemSid: number = 0) => {
+    if (!itemId) {
+      return;
     }
 
+    const savedList = JSON.parse(localStorage.getItem('items') as string);
+    let updatedList:any[] = [];
+    
+    // check if its already saved
+    const saved = savedList?.find((item:any) => item.id === itemId && item.sid === itemSid);
+    if (saved && savedList) {
+      updatedList = savedList?.filter((item:any) => item.id !== itemId && item.sid !== itemSid);
+    } else {
+      updatedList = [
+        ...savedList ?? [],
+        {
+          id: itemId,
+          sid: itemSid,
+        },
+      ];
+    }
+
+    localStorage.setItem('items', JSON.stringify(updatedList));
+  }
+
+  useEffect(() => {
+    setSavedLoading(true);
+
+    async function getAPIData() {
+      const savedList = JSON.parse(localStorage.getItem('items') as string);
+      if (savedList) {
+        const apiResponse = [];
+  
+        for (const savedItem of savedList) {  
+          console.log(savedItem)
+          apiResponse.push({
+            historical: await getHistoricalData(savedItem.id, savedItem.sid),
+            market: (await getMarketData(savedItem.id))[0],
+          });
+        }
+  
+        setBookmarkedItems(apiResponse);
+        setSavedLoading(false);
+      }
+    }
+  
     getAPIData();
-  }, []);
+  }, []);  
 
   return (
     <div>
       <Head>
         <title>Dina - A BDO Dashboard</title>
         <meta name='description' content='Simple Black Desert Online tracker' />
+        <meta name='author' content='ln2r' />
       </Head>
 
       <main>
         <NavbarComponent pageLocation='home' />
-        <Container className='p-4' fluid='true'>
+        <Container className='p-4' fluid={true}>
           <Row>
-            <Col xl={9}>
-              <Row>
-                {savedDataPrimary.map((data:any) => {
-                  return (
-                    <Col key={`market-card-${data.market.id}`}>
-                      <MarketCardComponent title={data.market.name} basePrice={data.market.basePrice} lastSold={data.market.lastSoldPrice} chartData={data.historical}/>
-                    </Col>
-                  )
-                })}
-              </Row>
-              <Row className='pt-3'>
-                {savedDataSecondary.map((data:any) => {
-                  return (
-                    <Col key={`market-card-${data.market.id}`}>
-                      <MarketCardComponent title={data.market.name} basePrice={data.market.basePrice} lastSold={data.market.lastSoldPrice} chartData={data.historical}/>
-                    </Col>
-                  )
-                })}
-              </Row>
+            <Col md={9} style={{ height: '85vh' }}>
+              <MarketCardComponent data={bookmarkedItems} />
             </Col>
-            <Col className='border-start' fluid>
+            <Col className='border-start' md={3} style={{ height: '85vh' }}>
               <Row>
                 <Form>
                   <Form.Control
                     type='search'
-                    placeholder='Search for an item...'
+                    placeholder='Search for items...'
                     className='me-2'
                     aria-label='Search'
                     onChange={searchItems}
@@ -205,7 +198,7 @@ export default function Home() {
                     { searchResults.map((result:any) => {
                       return (
                         <tr key={`search-result-${result.id}`}>
-                          <td className='ps-3'><a onClick={() => handleShow(result.id)}><FontAwesomeIcon icon={faStarRegular} size='sm' /> {new DOMParser().parseFromString(result.name, 'text/html').body.textContent}</a></td>
+                          <td className='ps-3'><a onClick={() => handleShow(result.id)}><FontAwesomeIcon icon={result.saved ? faStar : faStarRegular } size='sm' /> {new DOMParser().parseFromString(result.name, 'text/html').body.textContent}</a></td>
                         </tr>
                       )
                     })}
@@ -235,9 +228,9 @@ export default function Home() {
             ) : (
               <>
                 {itemModal?.market && (
-                  <>
-                    <FontAwesomeIcon icon={itemModal.market.name ? faStarRegular : faCircleXmark} size='sm' /> {itemModal.market.name ? itemModal.market.name : 'Can\'t get Item Data'}
-                  </>
+                  <div onClick={() => addToLocalStorage(itemModal?.market?.id, itemModal?.market?.sid)}>
+                    <FontAwesomeIcon icon={itemModal.market.name ? itemModal.market.saved ? faStar : faStarRegular : faCircleXmark} size='sm' /> {itemModal.market.name ? itemModal.market.name : 'Can\'t get Item Data'}
+                  </div>
                 )}
               </>
             )}
@@ -263,7 +256,7 @@ export default function Home() {
                       {itemModal?.market?.priceMax}
                     </p>
                     <p>
-                      <span className='fw-semibold'>Last Sold (1623625787)</span> {itemModal?.market?.lastSoldPrice}
+                      <span className='fw-semibold'>Last Sold ({new Date((itemModal?.market?.lastSoldTime ?? 0) * 1000).toLocaleString()})</span> {itemModal?.market?.lastSoldPrice}
                     </p>
                   </div>
                 </>
